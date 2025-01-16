47e664ea74fa54569c3c674995d1bf002068be0f
SeongHan <tjdgks9923@naver.com>
2025-01-16 14:29:00 +0900

    기본 설정값 추가

---
+++ moneytree_back/src/test/java/com/moneytree_back/MoneytreeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneytree_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytreeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
