ff30e5a36a042c09e69ad2476c95520fa88e012b
SeongHan <tjdgks9923@naver.com>
2025-01-17 10:52:21 +0900

    다시 파일 집어넣기

---
+++ moneytree_back/src/main/java/com/moneytree_back/MoneytreeBackApplication.java
@@ -0,0 +1,13 @@
+package com.moneytree_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytreeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneytreeBackApplication.class, args);
+    }
+
+}
