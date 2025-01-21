3cedf2f97b99b7fa62778ff9f4b511050f7e8d52
abc <syh2612@naver.com>
2025-01-21 11:38:05 +0900

    Revert "Merge branch 'feature/shin' of https://github.com/sau2120/MoneyTree into feature/shin"
    This reverts commit 628473059fdc5534c64510866bb300d5aad9f74a, reversing
    changes made to e4657b9cd15de4ed771cf61b3b5938f984906d13.

--- moneytree_back/src/main/java/com/moneytree_back/domain/DepositProduct.java
+++ moneytree_back/src/main/java/com/moneytree_back/domain/DepositProduct.java
@@ -5,7 +5,6 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +33,7 @@ public class DepositProduct {
     private String depositJoinWay;
 
     @Column(name = "deposit_min_amount", precision = 15, scale =2)
-    private BigDecimal depositMinAmount;
+    private Double depositMinAmount;
 
     @Column(name = "deposit_maturity_period") // 만기 날짜로 할지 만기 기간으로 할지
     private Integer depositMaturityPeriod; // 개월
@@ -44,7 +43,7 @@ public class DepositProduct {
     private LocalDateTime depositProductCreatedAt;
 
     @UpdateTimestamp
-    @Column(name = "deposit_product_updated_at")
+    @Column(name = "deposit_product_created_at")
     private LocalDateTime depositProductUpdatedAt;
 
     @OneToMany(mappedBy = "depositProduct", cascade = CascadeType.ALL, orphanRemoval = true)
