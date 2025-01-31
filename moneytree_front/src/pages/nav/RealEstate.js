baa085057f2b2964d33535eb576253991a53653e
DESKTOP-JGMEDQ9\EZEN <djak97@naver.com>
2025-01-31 17:44:31 +0900

    부동산 메인화면 지도로 변경

--- moneytree_front/src/pages/nav/RealEstate.js
+++ moneytree_front/src/pages/nav/RealEstate.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import KakaoMap from '../estate/KakaoMap'; // 지도를 불러오기 위해 추가
 
 function RealEstate() {
+  const location = useLocation(); // 현재 경로 확인
+
   return (
     <div style={{ padding: '20px' }}>
       <h2>🏡 부동산 정보 페이지</h2>
-      <p>부동산 관련 정보를 검색하고 커뮤니티에 참여하세요.</p>
+      <p>부동산 관련 매물을 검색하고 커뮤니티에 참여하세요.</p>
 
       {/* 네비게이션 추가 */}
       <nav style={{ marginBottom: '20px' }}>
@@ -26,8 +29,8 @@ function RealEstate() {
         </Link>
       </nav>
 
-      {/* 하위 라우트 페이지를 렌더링 */}
-      <Outlet />
+      {/* 기본 화면을 KakaoMap(지도)으로 설정, 다른 페이지에서는 Outlet 렌더링 */}
+      {location.pathname === '/realestate' ? <KakaoMap /> : <Outlet />}
     </div>
   );
 }
